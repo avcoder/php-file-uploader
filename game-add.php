bcfb9c54a723976c4e918aa055d5456bd6138ff1
avcoder <code.rom7.6@gmail.com>
2021-07-07 10:34:55 -0400

    added title tag

--- game-add.php
+++ game-add.php
@@ -13,6 +13,7 @@ function db_queryAll($sql, $conn) {
     return $genres;
 }
 
+$title_tag = "Add Game";
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +22,7 @@ function db_queryAll($sql, $conn) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= $title_tag ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
